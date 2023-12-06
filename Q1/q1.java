cb6bb211ccb576a13bc1553ab08878800f274e50
darshanlahamage <80032609+darshanlahamage@users.noreply.github.com>
2023-12-06 12:00:04 +0530

    2nd commit

--- Q1/q1.java
+++ Q1/q1.java
@@ -1,9 +1,24 @@
-class q1{
-    public int maxProfit(int[] prices){
-       
-        return op;
-    }
-    public static void main(String [] args){
+public class q1 {
 
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        int lengthAfterRemoval = removeDuplicates(nums);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < lengthAfterRemoval; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int j = 1; 
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i]; 
+                j++; 
+            }
+        }
+        return j; 
     }
 }
